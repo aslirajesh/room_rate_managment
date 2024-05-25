4e7b9b96f5434818570c3978ad2766f2354c3684
aslirajesh <rajeshkumar9896@gmail.com>
2024-05-25 17:46:35 +0530

    Update admin.py

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import RoomRate, OverriddenRoomRate, Discount, DiscountRoomRate
 
-# Register your models here.
+
+@admin.register(RoomRate)
+class RoomRateAdmin(admin.ModelAdmin):
+    list_display = ('room_id', 'room_name', 'default_rate')
+    search_fields = ('room_name',)
+    ordering = ('room_id',)
+
+
+@admin.register(OverriddenRoomRate)
+class OverriddenRoomRateAdmin(admin.ModelAdmin):
+    list_display = ('room_rate', 'overridden_rate', 'stay_date')
+    search_fields = ('room_rate__room_name',)
+    list_filter = ('stay_date',)
+    ordering = ('room_rate', 'stay_date')
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('discount_id', 'discount_name', 'discount_type', 'discount_value')
+    search_fields = ('discount_name',)
+    ordering = ('discount_id',)
+
+
+@admin.register(DiscountRoomRate)
+class DiscountRoomRateAdmin(admin.ModelAdmin):
+    list_display = ('room_rate', 'discount')
+    search_fields = ('room_rate__room_name', 'discount__discount_name')
+    ordering = ('room_rate', 'discount')
